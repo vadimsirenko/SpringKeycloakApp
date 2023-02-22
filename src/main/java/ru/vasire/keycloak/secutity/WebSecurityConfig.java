65968abadef5ee98324a159c768b5c106b462109
Сиренко Вадим Федорович <Sirenko.VF@gazprom-neft.ru>
2023-02-22 13:44:21 +0300

    comments

--- src/main/java/ru/vasire/keycloak/secutity/WebSecurityConfig.java
+++ src/main/java/ru/vasire/keycloak/secutity/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package ru.vasire.keycloak.secutity;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -28,12 +29,11 @@ import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
 
     private final KeycloakLogoutHandler keycloakLogoutHandler;
     private final JwtAuthConverter jwtAuthConverter;
-
-
 
     public static final String ADMIN = "admin";
     public static final String USER = "user";
@@ -69,7 +69,14 @@ public class WebSecurityConfig {
     }
 
 
-
+    /**
+     * Объект сопоставления, реализующий инерфейс {@link  GrantedAuthoritiesMapper},
+     * который внедряется в уровень аутентификации для преобразования полномочий,
+     * сформированных в разделе описания разрешений для области (realm_access) сервиса Keycloak,
+     * в те, которые будут использоваться в объекте аутентификации.
+     * @return Возвращает список разрешений {@link  GrantedAuthority} из раздела описания разрешений для области (realm_access) сервиса Keycloak
+     * @see GrantedAuthority
+     */
     @Bean
     public GrantedAuthoritiesMapper userAuthoritiesMapperForKeycloak() {
         return authorities -> {
@@ -101,16 +108,16 @@ public class WebSecurityConfig {
         };
     }
 
-    Collection<GrantedAuthority> generateAuthoritiesFromClaim(Collection<String> roles) {
+    /**
+     * Формирование списка разрешений из списка ролей
+     * @param roles Список ролей
+     * @return Возвращает список разрешений {@link  GrantedAuthority}
+     * @see GrantedAuthority
+     */
+    private Collection<GrantedAuthority> generateAuthoritiesFromClaim(Collection<String> roles) {
         return roles.stream()
                 .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
                 .collect(Collectors.toList());
-    }
-
-    @Autowired
-    WebSecurityConfig(KeycloakLogoutHandler keycloakLogoutHandler, JwtAuthConverter jwtAuthConverter) {
-        this.keycloakLogoutHandler = keycloakLogoutHandler;
-        this.jwtAuthConverter = jwtAuthConverter;
     }
 
     @Bean
